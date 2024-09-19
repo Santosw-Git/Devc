2f79ceb21e163fd484d1acdc2654f25bc2a34515
Santosh Khadka <santoshkhadka2326@gmail.com>
2024-09-20 00:40:23 +0545

    user_model_added with password hashing and jwt creation

---
+++ src/models/user.model.js
@@ -0,0 +1,93 @@
+import mongoose from "mongoose";
+import bcrypt from "bcrypt";
+import { JsonWebTokenError } from "jsonwebtoken";
+
+const userSchema = new mongoose.Schema(
+    {
+        userName: {
+            type: String,
+            required: true,
+            unique: true,
+            lowercase: true,
+            trim : true,
+            index : true
+
+        },
+        email: {
+            type: String,
+            required: true,
+            unique: true,
+            lowercase: true,
+            trim : true,
+
+        },
+        fullName: {
+            type: String,
+            required: true,
+            trim: true,
+            index : true
+
+        },
+        avatar : {
+            type: String, //cloudinary url
+            required: true,
+        },
+        watchHistory : [
+            {
+                type : mongoose.Schema.Types.ObjectId,
+                ref : "Video"
+            }
+        ],
+        password :
+        {
+            type: String,
+            required: [true, "password is required"],
+        },
+        refreshToken : {
+            type: String,
+        }
+    },
+{timestamps: true});
+
+userSchema.pre("save", async function(next) {
+    if(!this.isModified("password")) return next();
+    this.password = bcrypt.hash(this.password,10)
+    next();
+
+})
+
+userSchema.methods.isPasswordCorrect = async function(password){
+    return await bcrypt.compare(password ,this.password)
+
+}
+
+userSchema.methods.generateAccessToken = function() {
+    return JsonWebTokenError.sign({
+        _id : this._id,
+        email : this.email,
+        userName : this.userName,
+        fullName : this.fullName,
+    },
+    process.env.ACCESS_TOKEN_SECRET,
+    {
+        expiresIn : process.env.ACCESS_TOKEN_EXPIRY
+
+    }
+)
+}
+
+userSchema.methods.generateRefreshToken = function() {
+    return JsonWebTokenError.sign({
+        _id : this._id,
+        email : this.email,
+        userName : this.userName,
+        fullName : this.fullName,
+    },
+    process.env.REFRESH_TOKEN_SECRET,
+    {
+        expiresIn : process.env.REFRESH_TOKEN_EXPIRY
+    }
+)
+}
+
+export const User = mongoose.model("User" , userSchema)
--- src/utils/ApiError.js
+++ src/utils/ApiError.js
@@ -2,7 +2,11 @@ class ApiError extends Error {
     constructor(
         statusCode,
         message = "Something went wrong",
-        error = [],
+        error = [], //When the ApiError class is instantiated 
+                    //(i.e., when an error occurs in the application and this error is thrown),
+                   // it automatically creates a stack trace via Error.captureStackTrace().
+                   // This built-in JavaScript function captures the current execution stack 
+                   //and assigns it to the stack property of the error object.
         stack = ""
     ){
         super(message)
