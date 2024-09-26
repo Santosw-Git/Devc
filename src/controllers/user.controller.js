075e33ef6015100d3be81bb912755f83d6e9b3c3
Santosh Khadka <santoshkhadka2326@gmail.com>
2024-09-26 23:37:49 +0545

    getting Current user route and controller done

--- src/controllers/user.controller.js
+++ src/controllers/user.controller.js
@@ -229,9 +229,20 @@ const changeCurrentPassword = asyncHandler(async(req,res) => {
 
 
 })
+
+const getCurrentUser = asyncHandler(async(req,res) => {
+    return res.status(200)
+    .json(new ApiResponse(
+        200 ,
+        req.user , 
+        "User fetched successfully"
+        ))
+})
 export {
     registerUser,
     loginUser,
     logoutUser,
-    refreshAccessToken
+    refreshAccessToken,
+    changeCurrentPassword,
+    getCurrentUser
 }
